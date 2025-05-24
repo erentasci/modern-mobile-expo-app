7405221d8246e89ff017688aaf664f593c34a657
Eren Taşçı <erent4sci@gmail.com>
2025-05-25 02:16:16 +0300

    feat: created rightActions draft component

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Stack, useRouter } from 'expo-router';
 import { useEffect, useRef } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, TouchableOpacity, View } from 'react-native';
 import ReanimatedSwipeable, {
   SwipeableMethods,
 } from 'react-native-gesture-handler/ReanimatedSwipeable';
@@ -12,6 +13,19 @@ import Title from '@/components/Title';
 import { COLORS } from '@/lib/contants';
 import { useListStore } from '@/store/listStore';
 import { List } from '@/types';
+
+const renderRightActions = () => {
+  return (
+    <View className="flex-row items-center justify-end gap-2 pl-4 pr-2">
+      <TouchableOpacity className="rounded-full bg-neutral-200 p-2">
+        <Ionicons name="trash-outline" size={24} color="red" />
+      </TouchableOpacity>
+      <TouchableOpacity className="rounded-full bg-neutral-200 p-2">
+        <Ionicons name="pencil-outline" size={24} color="blue" />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default function Home() {
   const router = useRouter();
@@ -42,7 +56,10 @@ export default function Home() {
         data={lists}
         contentContainerClassName="gap-4 mt-4"
         renderItem={({ item, index }: { item: List; index: number }) => (
-          <ReanimatedSwipeable ref={reanimatedRef}>
+          <ReanimatedSwipeable
+            ref={reanimatedRef}
+            renderRightActions={renderRightActions}
+            rightThreshold={100}>
             <ListItem
               title={item.name.toString()}
               link={`lists/${item.id}`}
