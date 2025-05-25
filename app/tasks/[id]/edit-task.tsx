db52cc8fe43e6004b6e512ea3f206bdd0470947a
Eren Taşçı <erent4sci@gmail.com>
2025-05-25 18:27:27 +0300

    feat(task-edit): completed page and functions

--- app/tasks/[id]/edit-task.tsx
+++ app/tasks/[id]/edit-task.tsx
@@ -1,38 +1,212 @@
-import { ScrollView, View } from 'react-native';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { Alert, ScrollView, View } from 'react-native';
 
 import { Button } from '@/components/Button';
 import Container from '@/components/Container';
+import ErrorText from '@/components/ErrorText';
 import Input from '@/components/Input';
 import Title from '@/components/Title';
+import { TaskFormData, taskSchema } from '@/lib/validators/taskSchema';
+import { updateTask } from '@/queries/tasks';
+import { useTaskStore } from '@/store/taskStore';
 
 const Page = () => {
+  const { id } = useLocalSearchParams();
+  const { getTaskById, fetchTasksByListId } = useTaskStore();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<TaskFormData>({
+    resolver: zodResolver(taskSchema),
+  });
+  const [currentListId, setCurrentListId] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchCurrentTask = async () => {
+      if (id) {
+        const currentTask = await getTaskById(Number(id));
+        setCurrentListId(currentTask?.list_id || 0);
+        reset({
+          name: currentTask?.name,
+          description: currentTask?.description || '',
+          image: currentTask?.image || '',
+          status:
+            currentTask?.status === 'pending' ||
+            currentTask?.status === 'in_progress' ||
+            currentTask?.status === 'completed'
+              ? currentTask.status
+              : 'pending',
+          priority:
+            currentTask?.priority === 'low' ||
+            currentTask?.priority === 'medium' ||
+            currentTask?.priority === 'high'
+              ? currentTask.priority
+              : 'low',
+          is_completed: currentTask?.is_completed ? 'true' : 'false',
+          due_date: currentTask?.due_date ?? undefined,
+        });
+      }
+    };
+
+    fetchCurrentTask();
+  }, [id]);
+
+  const onSubmit = async (FormData: TaskFormData) => {
+    try {
+      const response = await updateTask(Number(id), {
+        name: FormData.name,
+        description: FormData.description || undefined,
+        image: FormData.image || undefined,
+        status: FormData.status || undefined,
+        priority: FormData.priority || undefined,
+        is_completed: FormData.is_completed === 'true',
+        due_date: FormData.due_date || undefined,
+      });
+
+      if (response?.changes > 0) {
+        Alert.alert('Success', 'Task updated successfully');
+        fetchTasksByListId(currentListId);
+      } else {
+        Alert.alert('Error', 'Failed to update the task');
+      }
+      //eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      Alert.alert('Error', 'An unexpected error occurred while editing list');
+    }
+  };
   return (
     <Container>
       <Title title="Edit Task" onBackPress />
       <ScrollView contentContainerClassName="flex flex-col gap-3 pb-6">
-        <View className="h-12">
-          <Input placeHolderText="Name" placeholderTextColor="text-neutral-500" />
-        </View>
-        <View className="h-12">
-          <Input placeHolderText="Description" placeholderTextColor="text-neutral-500" />
-        </View>
-        <View className="h-12">
-          <Input placeHolderText="Image" placeholderTextColor="text-neutral-500" />
-        </View>
-        <View className="h-12">
-          <Input placeHolderText="Status" placeholderTextColor="text-neutral-500" />
-        </View>
-        <View className="h-12">
-          <Input placeHolderText="Priority" placeholderTextColor="text-neutral-500" />
-        </View>
-        <View className="h-12">
-          <Input placeHolderText="Is Completed" placeholderTextColor="text-neutral-500" />
-        </View>
-        <View className="h-12">
-          <Input placeHolderText="Due Date" placeholderTextColor="text-neutral-500" />
-        </View>
-
-        <Button className="rounded-md bg-green-500 shadow-sm" title="Edit Task" />
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="h-12">
+              <Input
+                placeHolderText="Name"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor="text-neutral-500"
+              />
+            </View>
+          )}
+          name="name"
+          rules={{ required: true }}
+        />
+        {errors.name && <ErrorText message={errors.name.message} />}
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="h-12">
+              <Input
+                placeHolderText="Description"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor="text-neutral-500"
+              />
+            </View>
+          )}
+          name="description"
+          rules={{ required: true }}
+        />
+        {errors.description && <ErrorText message={errors.description.message} />}
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="h-12">
+              <Input
+                placeHolderText="Image"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor="text-neutral-500"
+              />
+            </View>
+          )}
+          name="image"
+          rules={{ required: true }}
+        />
+        {errors.image && <ErrorText message={errors.image.message} />}
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="h-12">
+              <Input
+                placeHolderText="Status"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor="text-neutral-500"
+              />
+            </View>
+          )}
+          name="status"
+          rules={{ required: true }}
+        />
+        {errors.status && <ErrorText message={errors.status.message} />}
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="h-12">
+              <Input
+                placeHolderText="Priority"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor="text-neutral-500"
+              />
+            </View>
+          )}
+          name="priority"
+          rules={{ required: true }}
+        />
+        {errors.priority && <ErrorText message={errors.priority.message} />}
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="h-12">
+              <Input
+                placeHolderText="Is Completed"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor="text-neutral-500"
+              />
+            </View>
+          )}
+          name="is_completed"
+          rules={{ required: true }}
+        />
+        {errors.is_completed && <ErrorText message={errors.is_completed.message} />}
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className="h-12">
+              <Input
+                placeHolderText="Due Date"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor="text-neutral-500"
+              />
+            </View>
+          )}
+          name="due_date"
+          rules={{ required: true }}
+        />
+        {errors.due_date && <ErrorText message={errors.due_date.message} />}
+        <Button
+          className="rounded-md bg-green-500 shadow-sm"
+          title="Edit Task"
+          onPress={handleSubmit(onSubmit)}
+        />
       </ScrollView>
     </Container>
   );
--- store/taskStore.ts
+++ store/taskStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-import { createTask, deleteTask, getTasksByListId } from '@/queries/tasks';
+import { createTask, deleteTask, getTaskById, getTasksByListId, updateTask } from '@/queries/tasks';
 import { Task } from '@/types';
 
 export interface TaskState {
@@ -12,6 +12,11 @@ export interface TaskState {
   ) => Promise<{ success: boolean; message?: string }>;
   fetchTasksByListId: (id: number) => Promise<{ success: boolean; message?: string }>;
   deleteTaskById: (id: number) => Promise<{ success: boolean; message?: string }>;
+  editTaskById: (
+    id: number,
+    task: Omit<Task, 'id'>
+  ) => Promise<{ success: boolean; message?: string }>;
+  getTaskById: (id: number) => Promise<Task | null>;
 }
 
 export const useTaskStore = create<TaskState>((set) => ({
@@ -85,6 +90,49 @@ export const useTaskStore = create<TaskState>((set) => ({
       }
     } catch {
       return { success: false, message: 'An unexpected error occurred while deleting the list ' };
+    }
+  },
+  editTaskById: async (id: number, task: Omit<Task, 'id'>) => {
+    try {
+      const response = await updateTask(id, {
+        name: task.name,
+        description: task.description ?? undefined,
+        image: task.image ?? undefined,
+        status: task.status ?? undefined,
+        priority: task.priority ?? undefined,
+        is_completed: task.is_completed ?? undefined,
+        due_date: task.due_date ?? undefined,
+      });
+      if (response.changes > 0) {
+        const currentTime = new Date().toISOString();
+        const updatedTask: Task = {
+          ...task,
+          id,
+          updated_at: currentTime,
+        };
+        set((state) => ({
+          tasks: state.tasks.map((task) => (task.id === id ? updatedTask : task)),
+        }));
+        return { success: true, message: 'Task updated successfully' };
+      } else {
+        return { success: false, message: 'Failed to update the task' };
+      }
+    } catch {
+      return { success: false, message: 'An unexpected error occurred while updating the task' };
+    }
+  },
+  getTaskById: async (id: number) => {
+    try {
+      const response = await getTaskById(id);
+      if (response) {
+        return response;
+      } else {
+        console.log('Failed to fetch task by ID');
+        return null;
+      }
+    } catch (error) {
+      console.error('An unexpected error occurred while fetching the task by ID:', error);
+      return null;
     }
   },
 }));
